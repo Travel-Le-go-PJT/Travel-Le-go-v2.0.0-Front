e96904b683da85e8c0d49c9c7b33d008bf1b934c
jhp336 <jhp336@naver.com>
2023-05-25 02:29:28 +0900

    feat : 홈화면 핫플레이스, 베스트 인포, 베스트 플랜 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <b-container fluid class="full-width-container">
       <b-row align-v="center">
-        <b-col col="4" md="4" class="text-center full-width-left">
+        <b-col cols="4" md="4" class="text-center full-width-left">
           <div id="title">
             <div class="centered-div">
               <router-link to="/">
@@ -19,7 +19,7 @@
             </div>
           </div>
         </b-col>
-        <b-col col="12" md="8" class="full-width-right">
+        <b-col cols="12" md="8" class="full-width-right">
           <div class="video-wrapper">
             <video ref="videoPlayer" muted autoplay loop>
               <source :src="currentVideoSource" type="video/mp4" />
@@ -39,79 +39,60 @@
           ></b-progress>
         </div>
       </div>
-      <div id="hotplace"></div>
     </b-container>
-    <div class="home">
-      <swiper class="swiper" :options="swiperOption">
-        <template v-for="attraction in bestAttractions">
-          <swiper-slide :key="attraction.contentId">
-            <img :src="attraction.image" width="300" height="300" />
-          </swiper-slide>
-        </template>
+    <div class="row slider hot-place">
+      <div class="col-12 text-center">
+        <h1>HOT PLACE</h1>
+        <swiper class="swiper" :options="hotOption">
+          <template v-for="attraction in bestAttractions">
+            <swiper-slide :key="attraction.contentId">
+              <hot-place-item :attraction="attraction" />
+            </swiper-slide>
+          </template>
 
-        <div class="swiper-button-prev" slot="button-prev"></div>
-        <div class="swiper-button-next" slot="button-next"></div>
-      </swiper>
-      <swiper class="swiper" :options="swiperOption">
-        <template v-for="article in bestPlans">
-          <swiper-slide :key="article.articleNo">
-            <trip-plan-item
-              :article="article"
-              :key="article.articleNo"
-            ></trip-plan-item>
-          </swiper-slide>
-        </template>
-        <div class="swiper-button-prev" slot="button-prev"></div>
-        <div class="swiper-button-next" slot="button-next"></div>
-      </swiper>
-      <swiper class="swiper" :options="swiperOption">
-        <template v-for="article in bestInfos">
-          <swiper-slide :key="article.articleNo">
-            <trip-plan-item
-              :article="article"
-              :key="article.articleNo"
-            ></trip-plan-item>
-          </swiper-slide>
-        </template>
-        <div class="swiper-button-prev" slot="button-prev"></div>
-        <div class="swiper-button-next" slot="button-next"></div>
-      </swiper>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
     </div>
+    <div class="row slider best-plan">
+      <div class="col-12 text-center">
+        <h1>BEST PLAN</h1>
+        <swiper class="swiper" :options="planOption">
+          <template v-for="article in bestPlans">
+            <swiper-slide :key="article.articleNo">
+              <best-plan-item :article="article"></best-plan-item>
+            </swiper-slide>
+          </template>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+    </div>
+    <div class="row slider best-info">
+      <div class="col-12 text-center">
+        <h1>BEST INFO</h1>
+        <swiper class="swiper" :options="infoOption">
+          <swiper-slide>
+            <div class="cardbox m-4">
+              <div class="card">
+                <div class="card-body">
+                  <h2 class="card-title">회원들의 여행 꿀정보를 받아가세요!</h2>
 
-    <swiper class="swiper" :options="swiperOption">
-      <template v-for="attraction in bestAttractions">
-        <swiper-slide :key="attraction.contentId">
-          <img :src="attraction.image" width="300" height="300" />
-        </swiper-slide>
-      </template>
-
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </swiper>
-    <swiper class="swiper" :options="swiperOption">
-      <template v-for="article in bestPlans">
-        <swiper-slide :key="article.articleNo">
-          <trip-plan-item
-            :article="article"
-            :key="article.articleNo"
-          ></trip-plan-item>
-        </swiper-slide>
-      </template>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </swiper>
-    <swiper class="swiper" :options="swiperOption">
-      <template v-for="article in bestInfos">
-        <swiper-slide :key="article.articleNo">
-          <trip-plan-item
-            :article="article"
-            :key="article.articleNo"
-          ></trip-plan-item>
-        </swiper-slide>
-      </template>
-      <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-    </swiper>
+                  <p class="card-text">가장 많은 조회수를 기록한 글들이에요.</p>
+                </div>
+              </div>
+            </div>
+          </swiper-slide>
+          <template v-for="article in bestInfos">
+            <swiper-slide :key="article.articleNo">
+              <best-info-item :article="article"></best-info-item>
+            </swiper-slide>
+          </template>
+          <div class="swiper-scrollbar"></div>
+        </swiper>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -119,14 +100,18 @@
 import http from "@/api/http";
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
-import TripPlanItem from "@/components/tripplan/TripPlanItem.vue";
+import BestPlanItem from "@/components/home/BestPlanItem.vue";
+import HotPlaceItem from "@/components/home/HotPlaceItem.vue";
+import BestInfoItem from "@/components/home/BestInfoItem.vue";
 
 export default {
   name: "HomeView",
   components: {
     Swiper,
     SwiperSlide,
-    TripPlanItem,
+    BestPlanItem,
+    HotPlaceItem,
+    BestInfoItem,
   },
   data() {
     return {
@@ -156,13 +141,65 @@ export default {
       bestPlans: [],
       bestInfos: [],
       slideIndex: 0,
-      swiperOption: {
+      planOption: {
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 20,
         loop: false,
+        breakpoints: {
+          "@0.75": {
+            slidesPerView: 1,
+          },
+          "@1.00": {
+            slidesPerView: 2,
+          },
+          "@1.50": {
+            slidesPerView: 3,
+          },
+        },
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
+        },
+      },
+      hotOption: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        loop: false,
+        breakpoints: {
+          "@0.75": {
+            slidesPerView: 1,
+          },
+          "@1.00": {
+            slidesPerView: 2,
+          },
+          "@1.50": {
+            slidesPerView: 3,
+          },
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+      infoOption: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        loop: false,
+        breakpoints: {
+          "@0.75": {
+            slidesPerView: 1,
+          },
+          "@1.00": {
+            slidesPerView: 2,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+          },
+        },
+        scrollbar: {
+          el: ".swiper-scrollbar",
+          draggable: true,
+          hide: false,
         },
       },
     };
@@ -208,9 +245,7 @@ export default {
       this.slideIndex = Math.min(maxIndex - 5, this.slideIndex + 5);
     },
     setRandomVideoIndex() {
-      this.currentVideoIndex = Math.floor(
-        Math.random() * this.videoSources.length
-      );
+      this.currentVideoIndex = Math.floor(Math.random() * this.videoSources.length);
     },
   },
   mounted() {
@@ -222,6 +257,14 @@ export default {
 };
 </script>
 <style scoped>
+@font-face {
+  font-family: "omyu_pretty";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2")
+    format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
 .full-width-container {
   width: 100% !important;
   height: 100vh !important;
@@ -279,5 +322,102 @@ video {
   top: 0;
   bottom: 0;
   height: 100%;
+}
+
+.slider {
+  width: 80vw;
+  margin: 0 auto;
+  text-align: center;
+  font-family: omyu_pretty;
+  font-size: 20px;
+}
+
+.swiper-container {
+  width: 70vw;
+}
+
+h1 {
+  margin-top: 20px;
+  text-decoration: underline;
+}
+
+.swiper-button-prev,
+.swiper-button-next {
+  background-color: #e5f3f2;
+  opacity: 0.5;
+  padding: 25px 25px;
+  border-radius: 20px;
+  color: #2790f9;
+}
+
+.swiper-button-prev:hover,
+.swiper-button-next:hover {
+  background-color: #8596aa;
+}
+
+.swiper-button-prev:after,
+.swiper-button-next:after {
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.hot-place {
+  margin-top: 100px;
+  margin-bottom: 140px;
+}
+
+.best-info {
+  margin-top: 140px;
+  margin-bottom: 40px;
+}
+
+.cardbox {
+  display: flex;
+  justify-content: space-around;
+}
+
+.card {
+  float: left;
+  width: 360px;
+  height: 320px;
+  border: none;
+}
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.card-button {
+  box-shadow: none;
+  font-size: 16px;
+  background-color: #2790f9;
+  border-color: #2790f9;
+  color: #fff;
+}
+
+.card-button:hover {
+  background-color: #1c5fa3;
+  border-color: #1c5fa3;
+  color: #fff;
+  box-shadow: none;
+}
+
+.swiper-scrollbar {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 6px;
+  background-color: #f1f1f1;
+  z-index: 1;
+}
+
+.swiper-scrollbar-drag {
+  position: absolute;
+  width: 0;
+  height: 100%;
+  background-color: #2790f9;
 }
 </style>
